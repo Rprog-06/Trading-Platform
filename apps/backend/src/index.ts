24749167c34aff89f0ce9dda7c2100bd8d897c11
Rizwan Vahora <vahorarizwan09@gmail.com>
2025-12-23 22:06:06 +0530

    as

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -8,3 +8,7 @@ const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`API Gateway running on port ${PORT}`);
 });
+app.get("/", (req, res) => {
+  res.send("Trading Platform API Gateway is running");
+});
+
